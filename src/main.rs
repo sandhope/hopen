b410cce1737e0dd7ee7e57877b3c6022440d21ab
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2025-01-06 18:05:12 +0800

    add init code

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+// Disable command line from opening on release mode
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use gpui::{div, prelude::*, rgb, App, AppContext, SharedString, ViewContext, WindowOptions};
+ 
+struct HelloWorld {
+    text: SharedString,
+}
+ 
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .bg(rgb(0x2e7d32))
+            .size_full()
+            .justify_center()
+            .items_center()
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
+    }
+}
+ 
+fn main() {
+    println!("111");
+    App::new().run(|cx: &mut AppContext| {
+        cx.open_window(WindowOptions::default(), |cx| {
+            cx.new_view(|_cx| HelloWorld {
+                text: "World".into(),
+            })
+        })
+        .unwrap();
+    });
+}
